b5247d30bd99f6b8ce7479a5ea06687f84e18f89
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-05-26 15:21:28 +0100

    Day 5

--- App1/main.py
+++ App1/main.py
@@ -1,19 +1,24 @@
 todos = []
 
 while True:
-    action = input("Type add[a], show[s], edit[e] or exit[x]:")
+    action = input("Type add[a], show[s], edit[e], complete[c] or exit[x]:")
 
     match action:
         case 'add' | 'a':
             todo = input("Enter a todo: ")
             todos.append(todo)
+
         case 'show' | 's':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1} - {item}"
+                print(row)
         case 'edit' | 'e':
             number = int(input("Option: "))
             new = input("New value: ")
-            todos[number-1] = new
+            todos[number - 1] = new
+        case 'complete' | 'c':
+            number = int(input("Option: "))
+            todos.pop(number - 1)
         case 'exit' | 'x':
             break
 
