a8699a6276df9a09a5009a32b681bc5870abfb40
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-06-06 18:25:29 +0100

    Day 11

--- App1/main.py
+++ App1/main.py
@@ -1,20 +1,24 @@
+def get_todos():
+    with open('files/todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
 while True:
     action = input("Type add, show, edit, complete or exit:")
+    action = action.strip()
 
-    if 'add' in action:
+    if action.startswith('add'):
         todo = action[4:] + "\n"
 
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
-
+        todos = get_todos()
         todos.append(todo)
 
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
 
-    elif 'show' in action:
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif action.startswith('show'):
+        todos = get_todos()
 
         # list comprehensions example
         # new_todos =[item.strip("\n") for item in todos]
@@ -23,35 +27,40 @@ while True:
             row = f"{index + 1} - {item}"
             print(row.strip("\n"))
 
-    elif 'edit' in action:
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif action.startswith('edit'):
+        try:
+            number = int(action[5:])
+            todos = get_todos()
 
-        number = int(action[5:])
-        new = input("New value: ")
-        todos[number - 1] = new + "\n"
+            new = input("New value: ")
+            todos[number - 1] = new + "\n"
 
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
 
-    elif 'complete' in action:
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif action.startswith('complete'):
+        try:
+            todos = get_todos()
 
-        number = int(action[9:])
-        todo_to_remove = todos[number - 1].strip("\ns")
-        todos.pop(number - 1)
+            number = int(action[9:])
+            todo_to_remove = todos[number - 1].strip("\n")
+            todos.pop(number - 1)
 
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
 
-        message = f"Todo {todo_to_remove} was removed from the list."
-        print(message)
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("Item doesn't exist.")
+            continue
 
-    elif 'exit' in action:
+    elif action.startswith('exit'):
         break
     else:
         print("Command not valid.")
 
 print("Bye")
-
