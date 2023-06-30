b21b5c5197e84dc47b33abedf475d8d5ee73f3fc
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-06-30 18:57:55 +0100

    Day 16

--- App1/main.py
+++ App1/main.py
@@ -1,21 +1,10 @@
-def get_todos(filepath='files/todos.txt'):
-    """
-    Get the todos from the text file
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+# from functions import get_todos, write_todos
+from modules import functions
+import time
 
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 
-def write_todos(todolist, filepath='files/todos.txt'):
-    """
-    Write the todos into the text file
-    """
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todolist)
-
-
-global_file_path = 'files/todos.txt'
 while True:
     action = input("Type add, show, edit, complete or exit:")
     action = action.strip()
@@ -23,13 +12,13 @@ while True:
     if action.startswith('add'):
         todo = action[4:] + "\n"
 
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append(todo)
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         # list comprehensions example
         # new_todos =[item.strip("\n") for item in todos]
@@ -41,12 +30,12 @@ while True:
     elif action.startswith('edit'):
         try:
             number = int(action[5:])
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new = input("New value: ")
             todos[number - 1] = new + "\n"
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -54,13 +43,13 @@ while True:
 
     elif action.startswith('complete'):
         try:
-            todos = get_todos()
+            todos = functions.get_todos()
 
             number = int(action[9:])
             todo_to_remove = todos[number - 1].strip("\n")
             todos.pop(number - 1)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
---
+++ App1/modules/functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "files/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Get the todos from the text file
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todolist, filepath=FILEPATH):
+    """
+    Write the todos into the text file
+    """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todolist)
+
+
+if __name__ == "__main__":
+    print(__name__)
+    print("Functions imported")
