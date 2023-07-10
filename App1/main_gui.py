601d8830b8573616e0e140134b953a4b5d1a2685
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-07-10 18:18:46 +0100

    Day 18

--- App1/main_gui.py
+++ App1/main_gui.py
@@ -1,6 +1,11 @@
 from modules import functions
 import PySimpleGUI as Sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 Sg.theme("Black")
 
